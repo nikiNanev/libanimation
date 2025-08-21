e507419830065e67235cc89f4b6936243a6f1e98
Nikolai Nanev <niki4etooo@gmail.com>
2025-08-21 10:42:28 +0300

    shared: created simple shared object test with cmake and minor changes to options/keyframes

--- shared/build/CMakeFiles/animation.dir/home/aceraspire5/Dev/Projects/Repos/Library/Animation/src/test.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/animation.dir/home/aceraspire5/Dev/Projects/Repos/Library/Animation/src/test.c.o: \
- /home/aceraspire5/Dev/Projects/Repos/Library/Animation/src/test.c \
- /usr/include/stdc-predef.h \
- /home/aceraspire5/Dev/Projects/Repos/Library/Animation/src/../include/test.h
